6471ab1ed6d5f45a0a6fb84f85cfba6e18b8d016
LindsayElia <lindsay.elia@gmail.com>
2016-07-25 14:46:34 -0700

    getting details for products on oneloveorganics

--- scrapers/sc-oneloveorganics.js
+++ scrapers/sc-oneloveorganics.js
@@ -1,4 +1,4 @@
-// Require Modules
+// require modules
 var request = require("request");
 var cheerio = require("cheerio");
 var fs = require("fs");
@@ -7,7 +7,6 @@ var fs = require("fs");
 // product list page - page(s) containing links to all the products
 // prouct detail page - page containing information about a single product
 var listOneLoveOrganics = "http://shop.oneloveorganics.com/";
-
 
 request(listOneLoveOrganics, function (error, response, listBody) {
 	if (error) {
@@ -23,29 +22,39 @@ request(listOneLoveOrganics, function (error, response, listBody) {
 		// console.log("count: " + count);
 		// console.log("linkObj: "+ linkObj);
 
-		var detailUrl = listOneLoveOrganics + $(linkObj).attr("href");
+		var detailUrl = listOneLoveOrganics + $listBody(linkObj).attr("href");
 
 		// making a second request here to each url
 		// this is where I can specify what data to pull, and only save what I might use
 		// save as json file type? what will be easiest for putting into a database?
 		request(detailUrl, function (err, res, detailBody) {
+			if (err) {
+				console.log("Request to " + detailUrl + " encountered an error: " + err);
+				return;
+			}
+
 			var $detailBody = cheerio.load(detailBody);
-			// var
+			var name = $detailBody(".title").text();
+			var price = $detailBody(".price").text();
+			var size = $detailBody(".prod_size span").text();
+			var imageUrl = $detailBody(".main-image a img").attr("src");
+			var imageAlt = $detailBody(".main-image a img").attr("alt");
+			var ingredientsGrouping = $detailBody("#tab3").html();
+			console.log(name + "/////" + price + "/////" + size + "/////" + imageUrl + "/////" + imageAlt + "/////" +  ingredientsGrouping + "....................................");
 
+			
 
-		}).pipe(fs.createWriteStream("brands/oneloveorganics/" + count + ".txt"));
+		});
 		console.log("url: " + detailUrl);
 	});
 
 });
 
 
-// to do - remove modules not used from package.json, such as x-ray
+// to do - remove modules not used from package.json, such as x-ray, stringify-safe
 
 
-
-
-
+//.pipe(fs.createWriteStream("scrapers/brands/oneloveorganics/" + count + ".txt"))
 
 
 
