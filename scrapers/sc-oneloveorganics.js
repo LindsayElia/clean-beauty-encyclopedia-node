bcbe128573d6efaac8d12edb8db8aeb4bd2d483c
LindsayElia <lindsay.elia@gmail.com>
2016-07-25 13:24:56 -0700

    clean up variable names

--- scrapers/sc-oneloveorganics.js
+++ scrapers/sc-oneloveorganics.js
@@ -1,37 +1,46 @@
 // Require Modules
 var request = require("request");
 var cheerio = require("cheerio");
-// var xray = require("x-ray")();
 var fs = require("fs");
 
 
-var productListOneLoveOrganics = "http://shop.oneloveorganics.com/";
+// product list page - page(s) containing links to all the products
+// prouct detail page - page containing information about a single product
+var listOneLoveOrganics = "http://shop.oneloveorganics.com/";
 
 
-request(productListOneLoveOrganics, function (error, response, body) {
+request(listOneLoveOrganics, function (error, response, listBody) {
 	if (error) {
-		console.log("Request to " + productListOneLoveOrganics + " encountered an error: " + error);
+		console.log("Request to " + listOneLoveOrganics + " encountered an error: " + error);
 		return;
 	}
 
-	console.log("Request to " + productListOneLoveOrganics + " successful");
+	console.log("Request to " + listOneLoveOrganics + " successful");
 	// load the body of the page into Cheerio so we can traverse the DOM
-	var $ = cheerio.load(body);
-	var links = $(".prod_box .p_img a");
+	var $listBody = cheerio.load(listBody);
+	var links = $listBody(".prod_box .p_img a");
 	links.each(function(count, linkObj){
 		// console.log("count: " + count);
 		// console.log("linkObj: "+ linkObj);
 
-		var url = productListOneLoveOrganics + $(linkObj).attr("href");
+		var detailUrl = listOneLoveOrganics + $(linkObj).attr("href");
 
-		// i'm making a second request here to each link.
-		// this is where I can specify what data to pull, and only save what i might use
+		// making a second request here to each url
+		// this is where I can specify what data to pull, and only save what I might use
 		// save as json file type? what will be easiest for putting into a database?
-		request(url).pipe(fs.createWriteStream("brands/oneloveorganics/" + count + ".txt"));
-		console.log("url: " + url);
+		request(detailUrl, function (err, res, detailBody) {
+			var $detailBody = cheerio.load(detailBody);
+			// var
+
+
+		}).pipe(fs.createWriteStream("brands/oneloveorganics/" + count + ".txt"));
+		console.log("url: " + detailUrl);
 	});
 
 });
+
+
+// to do - remove modules not used from package.json, such as x-ray
 
 
 
