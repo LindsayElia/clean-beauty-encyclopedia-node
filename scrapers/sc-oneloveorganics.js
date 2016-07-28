2cf421fdc493f701281602df3f474069b234237e
LindsayElia <lindsay.elia@gmail.com>
2016-07-28 15:45:49 -0700

    data is writing to a json file, cleaned up inputs to remove trailing spaces and html tags

--- scrapers/sc-oneloveorganics.js
+++ scrapers/sc-oneloveorganics.js
@@ -2,61 +2,80 @@
 var request = require("request");
 var cheerio = require("cheerio");
 var fs = require("fs");
-
+// var q = require("q");
+var sanitizeHtml = require("sanitize-html");
 
 // product list page - page(s) containing links to all the products
 // prouct detail page - page containing information about a single product
 var listOneLoveOrganics = "http://shop.oneloveorganics.com/";
 var productData = [];
 
-// first request
-request(listOneLoveOrganics, function (error, response, listBody) {
-	if (error) {
-		console.log("Request to " + listOneLoveOrganics + " encountered an error: " + error);
-		return;
-	}
-
-	console.log("Request to " + listOneLoveOrganics + " successful");
-	// load the body of the page into Cheerio so we can traverse the DOM
-	var $listBody = cheerio.load(listBody);
-	var links = $listBody(".prod_box .p_img a");
-	links.each(function(count, linkObj){
-		// console.log("count: " + count);
-		// console.log("linkObj: "+ linkObj);
-		var detailUrl = listOneLoveOrganics + $listBody(linkObj).attr("href");
-
-		// second request, to each url
-		// this is where I can specify what data to pull, and only save what I might use
-		// save as json file type? what will be easiest for putting into a database?
-		request(detailUrl, function (err, res, detailBody) {
-			if (err) {
-				console.log("Request to " + detailUrl + " encountered an error: " + err);
-				return;
-			}
-
-			var $detailBody = cheerio.load(detailBody);
-			var product = { name:"", price:"", size:"", imageUrl:"", imageAlt:"", ingredientsGrouping:"" };
-				product.name = $detailBody(".title").text();
-				product.price = $detailBody(".price").text();
-				product.size = $detailBody(".prod_size span").text();
-				product.imageUrl = $detailBody(".main-image a img").attr("src");
-				product.imageAlt = $detailBody(".main-image a img").attr("alt");
-				product.ingredientsGrouping = $detailBody("#tab3").html();
-			// console.log(name + "/////" + price + "/////" + size + "/////" + imageUrl + "/////" + imageAlt + "/////" +  ingredientsGrouping + "....................................");
-			console.log("product: " + product);
-			productData.push(product);
-			
-
+// first request, to get list of links to product detail pages
+var getList = function(listUrl){
+	request(listUrl, function (error, response, listBody) {
+		if (error) {
+			console.log("Request to " + listUrl + " encountered an error: " + error);
+			return;
+		}
+		// if no error, continue
+		// load the body of the page into cheerio so we can traverse the DOM
+		var $listBody = cheerio.load(listBody);
+		var links = $listBody(".prod_box .p_img a");
+		// need this function, below, to access the href inside of the a element, instead of the whole element
+		links.each(function(count, linkObj){
+			var detailUrl = listUrl + $listBody(linkObj).attr("href");
+			// call the getDetails function
+			getDetails(detailUrl);
 		});
-		console.log("url: " + detailUrl);
+	}).on('response', function(response){
+		console.log("getList responseStatusCode: " + response.statusCode);
+		console.log("getList responseHeaders: " + response.headers['content-type']);
 	});
+};
 
-});
+// second request, to each url
+var getDetails = function(detailUrl){
+	request(detailUrl, function (err, res, detailBody) {
+		if (err) {
+			console.log("Request to " + detailUrl + " encountered an error: " + err);
+			return;
+		}
+		// if no error, continue
+		var $detailBody = cheerio.load(detailBody);
+		var product = { name:"", price:"", size:"", imageUrl:"", imageAlt:"", ingredientsGrouping:"" };
+			product.name = ($detailBody(".title").text()).trim(); // trim() removes all extra spaces
+			product.price = ($detailBody(".price").text()).trim();
+			product.size = $detailBody(".prod_size span").text();
+			product.imageUrl = ($detailBody(".main-image a img").attr("src")).substr(2);
+			product.imageAlt = $detailBody(".main-image a img").attr("alt");
+			product.ingredientsGrouping = (sanitizeHtml(($detailBody("#tab3").html().trim()), {
+				allowedTags: [],
+				allowedAttributes: []
+			})).trim();
+		productData.push(product);
+	}).on('response', function(res){
+		console.log("getDetails responseStatusCode: " + res.statusCode);
+		console.log("getDetails responseHeaders: " + res.headers['content-type']);
+	});
+};
+
+var writeData = function(){
+	fs.writeFile("brands/oneloveorganics.json", JSON.stringify(productData, null, 4), function(){
+		console.log("value of productData from writeData: " + JSON.stringify(productData));
+		console.log("File for " + listOneLoveOrganics +" written");
+	});
+};
 
 
 
-//.pipe(fs.createWriteStream("scrapers/brands/oneloveorganics/" + count + ".txt"))
+getList(listOneLoveOrganics);
 
+// setTimeout is a cheat but it's what I can get to work for now.
+// need a better way to wait until the requests are all done, then calling writeData
+// look into using promises?
+setTimeout(function(){
+	writeData();
+}, 5000);
 
 
 
