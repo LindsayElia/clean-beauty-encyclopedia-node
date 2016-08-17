f8d09cb8dbfbcb34cded9f83a57242f1ca39d9bf
LindsayElia <lindsay.elia@gmail.com>
2016-08-17 16:00:36 -0700

    defining models and relationships

--- app/database/models.js
+++ app/database/models.js
@@ -1,11 +1,24 @@
+// connect to database with the existing bookshelf instance
+var bookshelf = require('./bookshelf');
+
+
 var Brands = bookshelf.Model.extend({
-	tableName: 'brands'
+	tableName: 'brands',
+	products: function(){
+		return this.hasMany(Products);
+	}
 });
 
 var Products = bookshelf.Model.extend({
-	tableName: 'products'
+	tableName: 'products',
+	
 });
 
 var Ingredients = bookshelf.Model.extend({
 	tableName: 'ingredients'
 });
+
+
+module.exports = Brands;
+module.exports = Products;
+module.exports = Ingredients;
